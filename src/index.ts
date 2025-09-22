d9be4657b2dd2ea25848ad2264783125e46112ea
sheikhlimon <sheikhlimon404@gmail.com>
2025-09-22 15:57:13 +0600

    added User schema and helper functions for model

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-const server = http.createServer();
+const server = http.createServer(app);
 
 server.listen(8080, () =>
   console.log("Server running on http://localhost:8080/"),
