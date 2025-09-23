9ec243e95755ecc273423e5cc802ad871cd0edef
sheikhlimon <sheikhlimon404@gmail.com>
2025-09-23 18:55:02 +0600

    Rename UserModel to user

--- src/db/users.ts
+++ src/db/users.ts
@@ -10,16 +10,16 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-export const UserModel = mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema);
 
-export const getUsers = () => UserModel.find();
-export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+export const getUsers = () => User.find();
+export const getUserByEmail = (email: string) => User.findOne({ email });
 export const getUserBySessionToken = (sessionToken: string) =>
-  UserModel.findOne({ "authentication.sessionToken": sessionToken });
-export const getUserById = (id: string) => UserModel.findById(id);
+  User.findOne({ "authentication.sessionToken": sessionToken });
+export const getUserById = (id: string) => User.findById(id);
 export const createUser = (values: Record<string, any>) =>
-  new UserModel(values).save().then((user) => user.toObject());
+  new User(values).save().then((user) => user.toObject());
 export const deleteUserById = (id: string) =>
-  UserModel.findOneAndDelete({ _id: id });
-export const updateUserById = (id: string, values: Record<string, any>) =>
-  UserModel.findByIdAndUpdate(id, values);
+  User.findOneAndDelete({ _id: id });
+export const updateUserById = (id: string) =>
+  User.findByIdAndUpdate({ _id: id });
